d027a6f06df1db90b8f372946a2794292f26dc84
ugurduyar <ismailugurduyar@gmail.com>
2020-06-06 02:34:55 +0300

    variables added

---
+++ script.js
@@ -0,0 +1,18 @@
+const wordEl = document.getElementById("word");
+const wrongLettersEl = document.getElementById("wrong-letters");
+const playAgainBtn = document.getElementById("play-button");
+const popup = document.getElementById("popup-container");
+const notification = document.getElementById("notification-container");
+const finalMessage = document.getElementById("final-message");
+
+const figureParts = document.querySelectorAll(".figure-part");
+
+const words = [
+  "tryhard",
+  "mustang",
+  "trial",
+  "hardwork",
+  "county",
+  "thinker",
+  "player",
+];
