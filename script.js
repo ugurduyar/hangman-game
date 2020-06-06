b6480eff52d349c8b026e59a784f7b66f1813a02
ugurduyar <ismailugurduyar@gmail.com>
2020-06-06 04:02:35 +0300

    css correction/letter check added

--- script.js
+++ script.js
@@ -46,4 +46,39 @@ function displayWord() {
   }
 }
 
+// Update the wrong letters
+function updateWrongLettersEl() {}
+
+// Show notification
+function showNotification() {
+  notification.classList.add("show");
+  setTimeout(() => {
+    notification.classList.remove("show");
+  }, 2000);
+}
+
+// Keydown letter press
+window.addEventListener("keydown", (e) => {
+  if (e.keyCode >= 65 && e.keyCode <= 90) {
+    const letter = e.key;
+
+    if (selectedWord.includes(letter)) {
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
+
+        displayWord();
+      } else {
+        showNotification();
+      }
+    } else {
+      if (!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter);
+        updateWrongLettersEl();
+      } else {
+        showNotification;
+      }
+    }
+  }
+});
+
 displayWord();
